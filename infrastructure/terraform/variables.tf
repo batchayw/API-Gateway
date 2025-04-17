d6b20d0330c7b9dacc0df835320249757686b284
william <batchayonwilliam@gmail.com>
2025-04-17 20:21:54 +0200

    Variables d’environnement

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -7,11 +7,59 @@ variable "cluster_name" {
 variable "node_count" {
   description = "Number of worker nodes"
   type        = number
-  default     = 3
+  default     = 5
+}
+
+variable "node_cpus" {
+  description = "Nombre de CPUs par nœud"
+  type        = number
+  default     = 4
+}
+
+variable "node_memory" {
+  description = "Mémoire par nœud (en MB)"
+  type        = number
+  default     = 16384
+}
+
+variable "namespace" {
+  description = "Namespace for deployment"
+  type        = string
+  default     = "gateway-system"
+}
+
+variable "gateway_class" {
+  description = "Gateway class name"
+  type        = string
+  default     = "istio"
 }
 
 variable "istio_version" {
   description = "Version of Istio to install"
   type        = string
   default     = "1.17.2"
+}
+
+variable "cert_manager_email" {
+  description = "Email for Let's Encrypt registration"
+  type        = string
+  default     = "admin@example.com"
+}
+
+variable "prometheus_retention" {
+  description = "Prometheus metrics retention period"
+  type        = string
+  default     = "7d"
+}
+
+variable "enable_ingress" {
+  description = "Enable NGINX Ingress Controller"
+  type        = bool
+  default     = false
+}
+
+variable "domain" {
+  description = "Base domain for ingress"
+  type        = string
+  default     = "example.com"
 }
